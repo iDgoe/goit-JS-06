f92d31905d63a23db809d7d12a2f7f5fe36c06a1
iDgoe <e.novykov@gmail.com>
2023-03-23 14:40:58 +0200

    1

--- js/task-03.js
+++ js/task-03.js
@@ -30,26 +30,3 @@ const result = x => {
 };
 
 list.insertAdjacentHTML('afterbegin', result(images));
-
-// const result = ingredients.map(element => {
-//   const el = document.createElement('li');
-//   el.textContent = element;
-//   el.classList.add('item');
-//   return el;
-// });
-
-// list.append(...result);
-
-// function result1(x) {
-//   return x
-//     .map(el => {
-//       return `<li class = 'item'>${el}</li>`;
-//     })
-//     .join('');
-// }
-
-// list.insertAdjacentHTML('afterbegin', result1(ingredients));
-
-// list.innerHTML = result1(ingredients);
-
-// list.innerHTML = '';
