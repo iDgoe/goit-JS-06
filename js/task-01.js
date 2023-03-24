15820efa07edbd9d3c0a724e3662e2f952167499
iDgoe <e.novykov@gmail.com>
2023-03-24 12:00:20 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,8 @@
-const itemAll = document.querySelectorAll('li.item');
+const itemAll = document.querySelectorAll('.item');
 console.log('Number of categories: ' + itemAll.length);
 
 itemAll.forEach(element => {
   console.log('Category: ' + element.firstElementChild.textContent);
   console.log('Elements: ' + element.querySelectorAll('li').length);
+  // console.log('Elements: ' + element.nextElementSibling.children.length);
 });
